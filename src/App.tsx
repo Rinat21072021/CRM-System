7e2afc6a82aeaf6993b663cdf6d42e2003ae0452
Rinat21072021 <tulendey@gmail.com>
2024-10-24 12:09:43 +0300

    feat/страница авторизации antD

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.scss';
+import { AuthPage } from './auth/Auth';
 import { TodoListPage } from './components/todolist/Todolist';
 
 function App() {
   return (
     <div className="App">
-      <TodoListPage />
+      <AuthPage/>
+      {/* <TodoListPage /> */}
+
     </div>
   );
 }
