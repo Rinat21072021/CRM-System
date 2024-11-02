3501c0a5f08a51bc42b8f634506d84dda61cccbe
Rinat <87980348+Rinat21072021@users.noreply.github.com>
2024-11-02 12:17:21 +0300

    Revert "feat/страница авторизации antD"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { Outlet } from 'react-router-dom';
 import './App.scss';
-
+import { TodoListPage } from './components/todolist/Todolist';
 
 function App() {
   return (
     <div className="App">
-      {/* <AuthPage/> */}
-      <Outlet />
-      {/* <TodoListPage /> */}
+      <TodoListPage />
     </div>
   );
 }
